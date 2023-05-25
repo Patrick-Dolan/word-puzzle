f7987a5218345093f9b92cd9caf79cb496c59f1d
Patrick Dolan <dolanp1992@gmail.com>
2023-05-25 15:34:58 -0700

    setup Puzzle class and test file

---
+++ WordPuzzleTests/ModelTests/PuzzleTests.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WordPuzzle.Models;
+
+namespace WordPuzzle.Tests
+{
+  [TestClass]
+  public class PuzzleTests 
+  {
+    [TestMethod]
+    public void Puzzle_CreatesInstanceOfPuzzle_Puzzle()
+    {
+      Puzzle newPuzzle = new Puzzle();
+      Assert.AreEqual(typeof(Puzzle), newPuzzle.GetType());
+    }
+  }
+}
