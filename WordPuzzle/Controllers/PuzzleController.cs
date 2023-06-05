84e73502a86878a8300305fd8f386bd186264750
Patrick Dolan <dolanp1992@gmail.com>
2023-06-05 13:09:13 -0700

    add game over conditions and view

--- WordPuzzle/Controllers/PuzzleController.cs
+++ WordPuzzle/Controllers/PuzzleController.cs
@@ -23,16 +23,32 @@ namespace WordPuzzle.Controllers
     public ActionResult Index(string userGuess, int searchId)
     {
       Puzzle foundPuzzle = Puzzle.Find(searchId);
+      if (userGuess == null)
+      {
+        return View(foundPuzzle);
+      }
       foundPuzzle.MakeGuess(userGuess);
-      
+
       // TODO REMOVE DEBUG TOOLS 
       // =================================================
       Console.WriteLine("User guess: " + userGuess);
       Console.WriteLine("User guess count: " + foundPuzzle.NumberOfGuesses);
       Console.WriteLine("User guess count from list: " + foundPuzzle.Guesses.Count);
       // =================================================
+      if (foundPuzzle.GameOver)
+      {
+        return RedirectToAction("GameOver", foundPuzzle);
+      }
+      else
+      {
+        return View(foundPuzzle);
+      }
+    }
 
-      return View(foundPuzzle);
+    [HttpGet("/GameOver")]
+    public ActionResult GameOver(Puzzle puzzle)
+    {
+      return View(puzzle);
     }
   }
 }
